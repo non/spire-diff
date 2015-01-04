0a6b278f52f4aaaabfa4882f646262a06ec313b9
Erik Osheim <d_m@plastic-idolatry.com>
2015-01-04 02:32:54 -0500

    Clean up Diff a bit more.
    Also, make DiffTest generic and test Byte, Int, String, and Char.

--- src/main/scala/spire/diff/Diff.scala
+++ src/main/scala/spire/diff/Diff.scala
@@ -5,19 +5,31 @@ import spire.syntax.cfor._
 import spire.syntax.eq._
 import spire.math.{min, max}
 
-import scala.reflect.ClassTag
 import scala.annotation.tailrec
+import scala.reflect.ClassTag
 
 object Diff {
 
-  def mkLeft(x0: Int, x1: Int): List[Slice] =
-    if (x0 < x1) SliceL(x0, x1) :: Nil else Nil
+  /**
+   * Diff the contents of `xs` and `ys`.
+   */
+  def diff[A: Eq: ClassTag](xs: IndexedSeq[A], ys: IndexedSeq[A]): Diff =
+    diff(xs.toArray, ys.toArray)
 
-  def mkRight(x0: Int, x1: Int): List[Slice] =
-    if (x0 < x1) SliceR(x0, x1) :: Nil else Nil
+  /**
+   * Diff the contents of `xs` and `ys`.
+   */
+  def diff[A: Eq](xs: Array[A], ys: Array[A]): Diff = {
 
-  def diff[A: Eq: ClassTag](xs: Array[A], ys: Array[A]): Diff = {
-    // some short circuits
+    // make a list with zero-or-one left slices
+    def mkLeft(x0: Int, x1: Int): List[Slice] =
+      if (x0 < x1) SliceL(x0, x1) :: Nil else Nil
+
+    // make a list with zero-or-one right slices
+    def mkRight(x0: Int, x1: Int): List[Slice] =
+      if (x0 < x1) SliceR(x0, x1) :: Nil else Nil
+
+    // some short circuits (empty arrays)
     if (xs.length == 0) return Diff(mkRight(0, ys.length))
     if (ys.length == 0) return Diff(mkLeft(0, xs.length))
 
@@ -26,7 +38,7 @@ object Diff {
     while (i < xs.length && i < ys.length && xs(i) === ys(i)) { i += 1 }
     val prefix = if (i > 0) SliceB(0, 0, i) :: Nil else Nil
 
-    // more possible short circuits
+    // more possible short circuits (one array contains the other)
     if (xs.length == i) {
       return Diff(prefix ::: mkRight(i, ys.length))
     } else if (ys.length == i) {
@@ -39,15 +51,22 @@ object Diff {
     while (j > i && k > i && xs(j) === ys(k)) { j -= 1; k -= 1 }
     val suffix = if (j < xs.length - 1) SliceB(j + 1, k + 1, xs.length - j - 1) :: Nil else Nil
 
-    // need to diff xs[i..j] with ys[j..k]
-    val slices = diffBody(Chunk(xs, i, j + 1), Chunk(ys, i, k + 1), i, suffix)
+    // need to diff xs[i, ..., j] with ys[i, ..., k]
+    val cxs = Chunk(xs, i, j + 1)
+    val cys = Chunk(ys, i, k + 1)
+    val matrix = Matrix.construct(cxs, cys)
+    val slices = matrix.trace(i, suffix)
     Diff(prefix ::: slices)
   }
-
-  def diffBody[A: Eq](xs: Chunk[A], ys: Chunk[A], offset: Int, suffix: List[Slice]): List[Slice] =
-    Matrix.construct(xs, ys).trace(offset, suffix)
 }
 
+/**
+ * Diff is just a wrapper around a list of slices.
+ * 
+ * Each slice says whether to read from the left, right, or both
+ * sources. Together, this list should describe how to quasi-optimally
+ * describe the differences between the left and right sources.
+ */
 case class Diff(slices: List[Slice]) {
 
   def display[A: Eq](xs: Array[A], ys: Array[A]): Unit = {
@@ -58,10 +77,12 @@ case class Diff(slices: List[Slice]) {
           slice match {
             case SliceB(x, y, len) =>
               cfor(0)(_ < len, _ + 1) { i =>
-                if (xs(x + i) === ys(y + i)) {
-                  println("=== " + xs(x + i).toString)
+                val a = xs(x + i)
+                val b = ys(y + i)
+                if (a === b) {
+                  println("=== " + a.toString)
                 } else {
-                  println("=!= " + xs(x + i).toString + " =!= " + ys(y + i).toString)
+                  println("=== " + a.toString + " =!= " + b.toString)
                 }
               }
             case SliceL(x0, x1) =>
--- src/test/scala/diff.scala
+++ src/test/scala/diff.scala
@@ -1,16 +1,22 @@
 package spire.diff
 
 import org.scalatest.matchers.ShouldMatchers
+import org.scalacheck.Arbitrary
 import org.scalacheck.Arbitrary._
 import org.scalatest._
 import prop._
 
+import spire.algebra.Eq
 import spire.implicits._
 
-class DiffCheck extends PropSpec with Matchers with GeneratorDrivenPropertyChecks {
-  val emptyArray = Array.empty[Int]
+import scala.reflect.ClassTag
 
-  def check[A](slices: List[Slice], xs: Array[A], ys: Array[A]): Boolean = {
+trait DiffCheck[A] extends PropSpec with Matchers with GeneratorDrivenPropertyChecks {
+  implicit def arb0: Arbitrary[A]
+  implicit def ct0: ClassTag[A]
+  implicit def eq0: Eq[A]
+
+  def check(slices: List[Slice], xs: Array[A], ys: Array[A]): Boolean = {
     def loop(slices: List[Slice], x: Int, y: Int): Option[(Int, Int)] =
       slices match {
         case Nil =>
@@ -31,11 +37,11 @@ class DiffCheck extends PropSpec with Matchers with GeneratorDrivenPropertyCheck
   }
 
   property("diff(nil, nil)") {
-    Diff.diff(emptyArray, emptyArray) shouldBe Diff(Nil)
+    Diff.diff(Array.empty[A], Array.empty[A]) shouldBe Diff(Nil)
   }
 
   property("diff(xs, xs)") {
-    forAll { (xs: Array[Int]) =>
+    forAll { (xs: Array[A]) =>
       val delta = Diff.diff(xs, xs)
       if (xs.isEmpty) delta.slices shouldBe Nil
       else delta.slices shouldBe List(SliceB(0, 0, xs.length))
@@ -43,32 +49,57 @@ class DiffCheck extends PropSpec with Matchers with GeneratorDrivenPropertyCheck
   }
 
   property("diff(xs, nil)") {
-    forAll { (xs: Array[Int]) =>
-      val delta = Diff.diff(xs, emptyArray)
+    forAll { (xs: Array[A]) =>
+      val delta = Diff.diff(xs, Array.empty[A])
       if (xs.isEmpty) delta.slices shouldBe Nil
       else delta.slices shouldBe List(SliceL(0, xs.length))
     }
   }
 
   property("diff(nil, xs)") {
-    forAll { (xs: Array[Int]) =>
-      val delta = Diff.diff(emptyArray, xs)
+    forAll { (xs: Array[A]) =>
+      val delta = Diff.diff(Array.empty[A], xs)
       if (xs.isEmpty) delta.slices shouldBe Nil
       else delta.slices shouldBe List(SliceR(0, xs.length))
     }
   }
 
   property("diff(xs, ys) should be ok") {
-    forAll { (xs: Array[Int], ys: Array[Int]) =>
+    forAll { (xs: Array[A], ys: Array[A]) =>
       val delta = Diff.diff(xs, ys)
       check(delta.slices, xs, ys) shouldBe true
     }
   }
 
   property("diffBody(xs, ys, ...) should be ok") {
-    forAll { (xs: Array[Int], ys: Array[Int]) =>
-      val slices = Diff.diffBody(Chunk(xs), Chunk(ys), 0, Nil)
+    forAll { (xs: Array[A], ys: Array[A]) =>
+      val m = Matrix.construct(Chunk(xs), Chunk(ys))
+      val slices = m.trace(0, Nil)
       check(slices, xs, ys) shouldBe true
     }
   }
+}
+
+class IntDiffCheck extends DiffCheck[Int] {
+  val arb0 = implicitly[Arbitrary[Int]]
+  val ct0 = implicitly[ClassTag[Int]]
+  val eq0 = Eq[Int]
+}
+
+class StringDiffCheck extends DiffCheck[String] {
+  val arb0 = implicitly[Arbitrary[String]]
+  val ct0 = implicitly[ClassTag[String]]
+  val eq0 = Eq[String]
+}
+
+class CharDiffCheck extends DiffCheck[Char] {
+  val arb0 = implicitly[Arbitrary[Char]]
+  val ct0 = implicitly[ClassTag[Char]]
+  val eq0 = Eq[Char]
+}
+
+class ByteDiffCheck extends DiffCheck[Byte] {
+  val arb0 = implicitly[Arbitrary[Byte]]
+  val ct0 = implicitly[ClassTag[Byte]]
+  val eq0 = Eq[Byte]
 }
